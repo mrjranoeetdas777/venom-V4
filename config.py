176bf674907243b8b1341e8516fd0f28cc7cb142
mrjranoeetdas777 <mr.ranojeetdas@gmail.com>
2024-07-26 10:38:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7211675268:AAEC9FRnv_GmyO26YAi3gku89_zTQuHtiyg")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("jeetddosbot")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
-
-
+ADMIN_IDS=("1232854923")
 
 
 
